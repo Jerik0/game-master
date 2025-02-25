3d07cb1811e7010f0aadbafa9002f19e3833c04c
Jonathan C <jcarrico1985@live.com>
2025-02-25 16:49:35 -0600

    create basic function structure to help with planning phase (hopefully?)

--- src/app/components/games/tetris/tetris.component.ts
+++ src/app/components/games/tetris/tetris.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-tetris',
@@ -6,28 +6,85 @@ import { Component } from '@angular/core';
   templateUrl: './tetris.component.html',
   styleUrl: './tetris.component.css'
 })
-export class TetrisComponent {
+export class TetrisComponent implements AfterViewInit {
+  @ViewChild('canvas') canvas: ElementRef<HTMLCanvasElement>;
+  context: CanvasRenderingContext2D;
   boardHeight = 600;
   boardWidth = 600;
+  // create pieces
+  lShapeRight = [
+    [[1],[0]],
+    [[1],[0]],
+    [[1],[0]],
+    [[1],[1]]
+  ];
+
+  lShapeLeft = [
+    [[0],[2]],
+    [[0],[2]],
+    [[0],[2]],
+    [[2],[2]]
+  ];
+
+  squareShape = [
+    [[3],[3]],
+    [[3],[3]]
+  ]
+
+  straightShape = [
+    [4],
+    [4],
+    [4],
+    [4]
+  ]
+
+  zShapeLeft = [
+    [[5],[5],[0]],
+    [[0],[5],[5]],
+  ]
+
+  zShapeRight = [
+    [[0],[5],[5]],
+    [[5],[5],[0]],
+  ]
+
+  tShape = [
+    [[0],[6],[0]],
+    [[6],[6],[6]],
+  ]
 
   // initialize the board
+  ngAfterViewInit(): void {
+    this.context = this.canvas.nativeElement.getContext('2d');
+  }
 
   // handle start game
+  startGame(): void {}
 
   // handle stop game
+  stopGame(): void {}
 
   // handle reset game
-
-  // create pieces
+  resetGame(): void {}
 
   // update the board with new frame
+  updateBoard(): void {}
+
+  render(): void {}
 
   // animate the board
+  animate(): void {}
 
   // randomly queue pieces to insert into board
+  insertPieces(): void {}
 
   // handle controlling of the pieces
+  handleMovement(event: KeyboardEvent): void {}
 
-  // handle board collision
+  translatePiece(): void {
+
+  }
+
+  // handle board collision detection
 
 }

c86e49bc36b92f8cbc58cc4e19c4dec6d9ed087a
Jonathan C <jcarrico1985@live.com>
2025-02-25 17:22:07 -0600

    created basic functionality for render, animate, updateScore, and setBoard methods.

--- src/app/components/games/tetris/tetris.component.ts
+++ src/app/components/games/tetris/tetris.component.ts
@@ -1,4 +1,5 @@
 import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {BitArray} from '../../../helpers/board-helpers';
 
 @Component({
   selector: 'app-tetris',
@@ -11,6 +12,12 @@ export class TetrisComponent implements AfterViewInit {
   context: CanvasRenderingContext2D;
   boardHeight = 600;
   boardWidth = 600;
+  score = 0;
+  gameBoard: BitArray[]
+  resolution = 4;
+  runGame = false;
+  rows = this.boardHeight / this.resolution;
+  columns = this.boardWidth / this.resolution;
   // create pieces
   lShapeRight = [
     [[1],[0]],
@@ -18,36 +25,30 @@ export class TetrisComponent implements AfterViewInit {
     [[1],[0]],
     [[1],[1]]
   ];
-
   lShapeLeft = [
     [[0],[2]],
     [[0],[2]],
     [[0],[2]],
     [[2],[2]]
   ];
-
   squareShape = [
     [[3],[3]],
     [[3],[3]]
   ]
-
   straightShape = [
     [4],
     [4],
     [4],
     [4]
   ]
-
   zShapeLeft = [
     [[5],[5],[0]],
     [[0],[5],[5]],
   ]
-
   zShapeRight = [
     [[0],[5],[5]],
     [[5],[5],[0]],
   ]
-
   tShape = [
     [[0],[6],[0]],
     [[6],[6],[6]],
@@ -67,13 +68,38 @@ export class TetrisComponent implements AfterViewInit {
   // handle reset game
   resetGame(): void {}
 
+  setBoard(): void {
+    this.gameBoard = new Array(this.rows).fill(0).map(() => new Array(this.columns).fill(0));
+  }
+
   // update the board with new frame
   updateBoard(): void {}
 
-  render(): void {}
+  updateScore(points: number): void {
+    this.score += points;
+  }
+
+  render(board: BitArray[]): void {
+    const c =  this.context;
+    const res = this.resolution;
+
+    board.forEach((row, rowIndex) => {
+      row.forEach((col, colIndex) => {
+        c.beginPath();
+        c.rect(colIndex * res, rowIndex * res, res, res);
+        c.fillStyle = '#f5f5f5'
+      })
+    })
+  }
 
   // animate the board
-  animate(): void {}
+  animate(timeoutInterval = 1000): void {
+    setTimeout(() => {
+      requestAnimationFrame(() => this.animate());
+    }, timeoutInterval);
+    this.render(this.gameBoard);
+    this.updateBoard();
+  }
 
   // randomly queue pieces to insert into board
   insertPieces(): void {}

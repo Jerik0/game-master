4927d2ebd01c016b0ddf85d563f90ee6133d411b
Jonathan C <jcarrico1985@live.com>
2025-02-25 16:27:57 -0600

    starting work on tetris now, and my intent is to get zero or as little help from the internet as possible for this and all remaining games in this project. Wish me luck :<

--- src/app/components/games/tetris/tetris.component.ts
+++ src/app/components/games/tetris/tetris.component.ts
@@ -7,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './tetris.component.css'
 })
 export class TetrisComponent {
+  boardHeight = 600;
+  boardWidth = 600;
+
+  // initialize the board
+
+  // handle start game
+
+  // handle stop game
+
+  // handle reset game
+
+  // create pieces
+
+  // update the board with new frame
+
+  // animate the board
+
+  // randomly queue pieces to insert into board
+
+  // handle controlling of the pieces
+
+  // handle board collision
 
 }
